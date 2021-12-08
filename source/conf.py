26850c50fa4ca279ba2d38df8542976a57c2974c
David Nind <david@davidnind.com>
2021-12-08 21:39:01 +1300

    Bug 29656: Change the color of the manual version number - HTML output
    The version number in the HTML output is hard to read. This patch adds
    custom css to override the theme css style.

--- source/conf.py
+++ source/conf.py
@@ -148,6 +148,9 @@ html_theme_options = {
 
 }
 
+html_css_files = {
+  'custom.css'
+}
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
 
@@ -174,7 +177,7 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['css/']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
